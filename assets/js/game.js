61f1db686bcabb7a961d15a59c38ccb34bb00237
Dheeru Singh <dheeru.singh228@gmail.com>
2019-06-23 20:27:37 -0400

    final commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,41 +1,41 @@
- // Initial array of animals
+// Initial array of animals
  
- var animalsArray = ["cat", "dog", "cow", "monkey"];
+var animalsArray = ["cat", "dog", "cow", "monkey"];
 
- // displayAnimalImages function re-renders the HTML to display the appropriate content
- function displayAnimalImages() {
-   reset();
-   var animalName = $(this).attr("data-name");
-   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName  + "&api_key=e6OFIzIJm8CK6O6aiWItgKxH4bKZTzjB&limit=12"
-   // Creates AJAX call for the specific animal button being clicked
-   $.ajax({
-     url: queryURL,
-     method: "GET"
-   }).then(function(response) {
-   // console.log(response);
-    //Print the 12 images af animal
-    for(var i=0; i<12; i++){   
-     $("#images-view").append("<div class='image-box'> <img class='gif' src='"+
-     response.data[i].images.downsized_still.url + "' data-still='"+
-     response.data[i].images.downsized_still.url +"' data-animate='"+
-     response.data[i].images.downsized_medium.url +"' data-state='still'/>" + 
-     "<div class='rating'>Rating : "+response.data[i].rating + "</div></div>");
+// displayAnimalImages function re-renders the HTML to display the appropriate content
+function displayAnimalImages() {
+  reset();
+  var animalName = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName  + "&api_key=e6OFIzIJm8CK6O6aiWItgKxH4bKZTzjB&limit=12"
+  // Creates AJAX call for the specific animal button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+  // console.log(response);
+  //Print the 12 images af animal
+  for(var i=0; i<12; i++){   
+    $("#images-view").append("<div class='image-box'> <img class='gif' src='"+
+    response.data[i].images.downsized_still.url + "' data-still='"+
+    response.data[i].images.downsized_still.url +"' data-animate='"+
+    response.data[i].images.downsized_medium.url +"' data-state='still'/>" + 
+    "<div class='rating'>Rating : "+response.data[i].rating + "</div></div>");
  
-    }
+  }
 
     //when we click gif images then the src attribute will be changed
     $(".gif").on("click", function() {
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
       });
-   });
+  });
 
  }
 //Reset function 
